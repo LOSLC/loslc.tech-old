e5994899658645c565e1ca36230a29bea96121ca
Frusadev <frusadev@gmail.com>
2025-08-04 12:02:30 +0000

    feat(frontend): add layouts and pages

---
+++ frontend/src/app/(main)/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Hero from "@/components/core/Hero";
+import AboutSection from "@/components/core/AboutSection";
+import Features from "@/components/core/Features";
+import CommunityStats from "@/components/core/CommunityStats";
+import Testimonials from "@/components/core/Testimonials";
+import CallToAction from "@/components/core/CallToAction";
+import Footer from "@/components/core/Footer";
+import { useRef } from "react";
+
+export default function Home() {
+  const aboutSectionRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div className="bg-background min-h-[940px] h-screen">
+      <div>
+        <Hero nextPageRef={aboutSectionRef} />
+      </div>
+      <AboutSection ref={aboutSectionRef} />
+      <Features />
+      <CommunityStats />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+}
