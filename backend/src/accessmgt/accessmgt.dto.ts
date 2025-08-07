1eb5cef9ffe53f29638967f59587b7e3ff1aebe3
Frusadev <frusadev@gmail.com>
2025-08-07 10:12:18 +0000

    Fix: Add expose Decorator to DTO classes non-exposed props

--- backend/src/accessmgt/accessmgt.dto.ts
+++ backend/src/accessmgt/accessmgt.dto.ts
@@ -1,6 +1,7 @@
 import { NewPermission, NewRole, Permission, Role } from "@/core/db/schema";
 import { IsNotEmpty, Max, Min } from "class-validator";
 import { ActionType, ResourceType } from "./accessmgt.types";
+import { Expose } from "class-transformer";
 
 interface GetUserRolesInterface {
   userId: string;
@@ -35,64 +36,107 @@ interface PermissionAssignmentDTOInterface {
 }
 
 export class GetUserRolesDTO implements GetUserRolesInterface {
+  @Expose()
   userId: string;
+  
+  @Expose()
   all?: boolean;
 
+  @Expose()
   @Max(100)
   limit?: number;
 
+  @Expose()
   @Min(0)
   offset?: number;
 }
 
 export class GetRolePermissionsDTO implements GetRolePermissionsInterface {
+  @Expose()
   roleId: string;
+  
+  @Expose()
   all?: boolean;
+  
+  @Expose()
   @Max(100)
   limit?: number;
+  
+  @Expose()
   @Min(0)
   offset?: number;
 }
 
 export class RoleDTO implements RoleDTOInterface {
+  @Expose()
   id: string;
+  
+  @Expose()
   name: string | null;
+  
+  @Expose()
   description: string | null;
+  
+  @Expose()
   createdAt: Date;
 }
 
 export class PermissionDTO implements PermissionDTOInterface {
+  @Expose()
   id: string;
+  
+  @Expose()
   name: string | null;
+  
+  @Expose()
   action: string;
+  
+  @Expose()
   resource: string;
+  
+  @Expose()
   resourceId: string | null;
+  
+  @Expose()
   description: string | null;
+  
+  @Expose()
   createdAt: Date;
 }
 
 export class CreateRoleDTO implements CreateRoleDTOInterface {
+  @Expose()
   name: string | null;
+  
+  @Expose()
   description: string | null;
 }
 
 export class CreatePermissionDTO implements CreatePermissionDTOInterface {
+  @Expose()
   name: string | null;
 
+  @Expose()
   @IsNotEmpty()
   action: ActionType;
 
+  @Expose()
   @IsNotEmpty()
   resource: ResourceType;
 
+  @Expose()
   resourceId: string | null;
+  
+  @Expose()
   description: string | null;
 }
 
 export class RoleAssignmentDTO implements RoleAssignmentDTOInterface {
+  @Expose()
   @IsNotEmpty()
   userId: string;
 
+  @Expose()
   @IsNotEmpty()
   roleId: string;
 }
@@ -100,9 +144,11 @@ export class RoleAssignmentDTO implements RoleAssignmentDTOInterface {
 export class PermissionAssignmentDTO
   implements PermissionAssignmentDTOInterface
 {
+  @Expose()
   @IsNotEmpty()
   roleId: string;
 
+  @Expose()
   @IsNotEmpty()
   permissionId: string;
 }
